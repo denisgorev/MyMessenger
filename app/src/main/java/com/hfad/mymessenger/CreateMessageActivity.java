d00007a2910c4b1b5ca0f943eda502a65933d213
dgorev002 <denisgorev1992@gmail.com>
2018-12-30 10:27:33 +0300

    ок

--- app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -15,14 +15,14 @@ public class CreateMessageActivity extends Activity {
     }
 
     public void onSendMessage (View view) {
-        EditText messageView = (EditText)findViewById(R.id.message);
-        String messageText = messageView.getText().toString();
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, messageText);
-        String chooserTitle = getString(R.string.chooser);
-        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
-        startActivity(chosenIntent);
+        EditText messageView = (EditText)findViewById(R.id.message); //создание нового объекта EditText с id message
+        String messageText = messageView.getText().toString(); //получение текста из объекта messageView
+        Intent intent = new Intent(Intent.ACTION_SEND); //создание нового интента с опцией отправки сообшений
+        intent.setType("text/plain"); //описание типа данных
+        intent.putExtra(Intent.EXTRA_TEXT, messageText); //включение дополнительной информации в интент (сообщение)
+        String chooserTitle = getString(R.string.chooser); //получение заголовка из Strings.xml для заголовка окна выбора приложения
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle); //createChooser возвращает объект Intent со всеми заданными свойствами, но с выводом окна выбора активности
+        startActivity(chosenIntent); //запуск активности, вызываемой интентом
 
     }
 }
